88029aaefd57b10552bc5f19c425bef901194815
Amaral Neto <alunoamaralneto@gmail.com>
2025-03-07 15:10:29 -0300

    Incluído conteudo

--- js/script.js
+++ js/script.js
@@ -1,33 +1,25 @@
-// let menuSetor = document.getElementById('menuSetor')
-// let setor = document.getElementById('setor')
+// SLIDE INICIO
+var radio = document.querySelector('.manual-btn')
+var cont = 1
 
+document.getElementById('radio1').checked = true
 
+setInterval(() => {
+    proximaImg()
+}, 5000)
 
-// function clickMenu() {
-//     if (menuSetor.style.display == 'none') {
-//         menuSetor.style.display = 'block'
-//     } else {
-//         menuSetor.style.display = 'none'
-//     }
-// }
+function proximaImg(){
+    cont++
 
-
-const carousel = document.querySelector(".carousel");
-let index = 0;
-
-function moveCarousel() {
-    index++;
-    if (index >= carousel.children.length) {
-        index = 0;
+    if(cont > 3){
+        cont = 1 
     }
-    let offset = -index * 100;
-    carousel.style.transform = `translateX(${offset}%)`;
+
+    document.getElementById('radio'+cont).checked = true
 }
+// SLIDE FIM
 
-setInterval(moveCarousel, 4000); // Troca a cada 3 segundos
-
-
-// MENU TOPO
+// MENU TOPO INICIO
 let burguer = document.getElementById('burguer')
 let menu = document.getElementById('menuAlto')
 function menuTopo() {
@@ -37,3 +29,67 @@ function menuTopo() {
         menu.style.display = 'block'
     }
 }
+
+// MENU TOPO FIM
+
+// JANELA MODAL INICIO
+function openModal(){
+    const modal = document.getElementById('modal-container')
+    modal.classList.add('mostrar')
+
+    modal.addEventListener('click', (e) =>{
+        if (e.target.id == 'modal-container' || e.target.id == "fechar"){
+            modal.classList.remove('mostrar')
+            localStorage.fechaModal = 'modal-container'
+        }
+    })
+}
+// JANELA MODAL FIM
+
+// JANELA MODAL INICIO 2
+function openModal2(){
+    const modal2 = document.getElementById('modal-container2')
+    modal2.classList.add('mostrar')
+
+    modal2.addEventListener('click', (e) =>{
+        if (e.target.id == 'modal-container2' || e.target.id == "fechar2"){
+            modal2.classList.remove('mostrar')
+            localStorage.fechaModal = 'modal-container2'
+        }
+    })
+}
+// JANELA MODAL FIM 2
+
+
+// MSG COOKIES
+
+var msgCookies = document.getElementById('cookies-msg')
+
+function aceito(){
+    localStorage.lgpd = 'sim'
+    msgCookies.classList.remove('mostrar')
+}
+
+if(localStorage.lgpd == 'sim'){
+    msgCookies.classList.remove('mostrar')
+}else{
+    msgCookies.classList.add('mostrar')
+}
+// MSG COOKIES FIM
+
+
+// MENU ABRIR E FECHAR
+const menuBurguer = document.getElementById('burguer')
+
+const menuLista = document.getElementById('menuAlto')
+
+function abrirMenu() {
+    if (menuLista.style.display === 'block') {
+        menuLista.style.display = 'none'
+    } else {
+        menuLista.style.display = 'block'
+    }
+}
+
+
+// MENU ABRIR E FECHAR FIM
